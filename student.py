c4860261899c98a13eebf1b7c6a50beb95a77e4b
bhavanachavana <bhavanachavana7@gmail.com>
2025-12-02 12:04:13 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,20 @@
+import  student
+
+def student_details(name, id, usn,course ,academic_year):
+    result = (
+        f"student Name : {name}\n"
+        f"student ID   : {id}\n"
+        f"usn    : {usn}\n"
+        f"course       : {course}\n"
+        f"academic_year  : {academic_year}\n"
+    )
+    return result
+
+
+if __name__ == "__main__":
+    name = "bhavana"
+    id = "013"
+    usn = "01fe24bca015"
+    course = "bca"
+    academic_year="2025-2027"
+    print(student_details(name,id, usn,course, academic_year))
