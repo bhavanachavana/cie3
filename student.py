7f4b63ed250298a36f98991f4616e4804cd51dcf
bhavanachavana <bhavanachavana7@gmail.com>
2025-12-02 12:36:35 +0530

    Refactor student_details formatting and main block

--- student.py
+++ student.py
@@ -3,18 +3,18 @@ import  student
 def student_details(name, id, usn,course ,academic_year):
     result = (
         f"student Name : {name}\n"
-        f"student ID   : {id}\n"
-        f"usn    : {usn}\n"
-        f"course       : {course}\n"
-        f"academic_year  : {academic_year}\n"
+        f"student ID  : {id}\n"
+        f"usn  : {usn}\n"
+        f"course  : {course}\n"
+        f"academic_year :{academic_year}\n"
     )
     return result
 
 
-if __name__ == "__main__":
-    name = "bhavana"
-    id = "013"
-    usn = "01fe24bca015"
-    course = "bca"
+if __name__=="__main__":
+    name ="bhavana"
+    id ="013"
+    usn ="01fe24bca015"
+    course ="bca"
     academic_year="2025-2027"
-    print(student_details(name,id, usn,course, academic_year))
+    print(student_details(name,id,usn,course,academic_year))
